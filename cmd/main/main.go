1892eee9f6d8ff2ee556020758debd422c97a78b
piwonka <christian_piwonka@aol.com>
2024-03-10 18:14:34 +0100

    Added creation of EDHRec appId and added godoc

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strings"
 
 	"fyne.io/fyne/v2"
@@ -18,7 +19,11 @@ import (
 )
 
 var PLACEHOLDER_IMAGE string = "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg/revision/latest?cb=20140813141013"
+var EDHREC_BASE_URL string = "https://edhrec.com"
 
+// Selects a random commander depending on the input constraints and fetches an image and a decklist for said commander
+// Params: An Array of strings containing all currently selected color checkboxes (and the "Exact" checkbox) from the UI
+// Returns: A Tuple of strings, the first of which being the image URI of the commander and the second being the decklist for the commander
 func GetCommanderImageAndDecklist(selected_colors []string) (string, string) {
 	fmt.Println("Building Query...")
 	var query string = BuildScryfallCommanderQuery(selected_colors)
@@ -37,15 +42,17 @@ func GetCommanderImageAndDecklist(selected_colors []string) (string, string) {
 	}
 }
 
+// Retrieves the average decklist for a given commander name from EDHRec.com
+// Params: The name of the commander the decklist shall be retrieved for
+// Returns: a Tuple containing a string, representing the average decklist for the commander and an error that is nil unless the retrieval was unsuccessful
 func GetEDHRecAvgDecklist(commander string) (string, error) {
-	// TODO: add computation of session token instead of hardcoded one ( this will work for about 24 hours after writing )
-	avg_deck_endpoint := "https://edhrec.com/_next/data/7-TtnLfoAX_AgebfCokAf/average-decks/" + commander + ".json?commanderName=" + commander
+	avg_deck_endpoint := EDHREC_BASE_URL + "/_next/data/" + GetBuildId() + "/average-decks/" + commander + ".json?commanderName=" + commander // 7-TtnLfoAX_AgebfCokAf
 	fmt.Println("Retrieving Deck from: " + avg_deck_endpoint)
-	page_resp, err := http.Get(avg_deck_endpoint) // TODO: add error handling
+	page_resp, err := http.Get(avg_deck_endpoint)
 	if err != nil {
 		return "", err
 	} else {
-		page_bytes, err := io.ReadAll(page_resp.Body) // TODO: add error handling
+		page_bytes, err := io.ReadAll(page_resp.Body)
 		defer page_resp.Body.Close()
 		if err != nil {
 			return "", err
@@ -60,6 +67,9 @@ func GetEDHRecAvgDecklist(commander string) (string, error) {
 	}
 }
 
+// Builds a Scryfall.com query using the selected constraints from the UI
+// Params: An Array of strings containing all currently selected color checkboxes (and the "Exact" checkbox) from the UI
+// Returns :  The complete scryfall api request with the query as a string
 func BuildScryfallCommanderQuery(selected_colors []string) string {
 	var query string = "https://api.scryfall.com/cards/random?q="
 	query += url.QueryEscape("is:Commander")                                                     // we only query for commanders
@@ -88,6 +98,9 @@ func BuildScryfallCommanderQuery(selected_colors []string) string {
 	}
 }
 
+// Parses the JSON data retrieved from a Scryfall card query and returns the pre-formatted cardname for EDHRec queries and the URI for a normal sized image of the card
+// Params: The json_data from the Scryfall API response as a string
+// Return: A tuple of strings containing the formatted card name and the image URI
 func ParseScryfallData(json_data string) (string, string) {
 	// get the cards name
 	var card_name string = gjson.Get(json_data, "name").String()
@@ -105,6 +118,9 @@ func ParseScryfallData(json_data string) (string, string) {
 	return formatted_card_name, image_uri
 }
 
+// Sends a HTTP GET request to scryfall using the given query and returns the response
+// Params: The url + query for the Scryfall API as a string
+// Returns a tuple containing the entire json response and an error if the retrieval failed. (+ empty string)
 func GetScryfallCommanderData(query string) (string, error) {
 	response, err := http.Get(query) // request json data from the scryfall rest api
 	if err != nil {                  // if this fails return the error
@@ -120,18 +136,47 @@ func GetScryfallCommanderData(query string) (string, error) {
 	}
 }
 
-func getImageResource(image_uri string) fyne.Resource {
+// Transforms an image URI into a fyne Resource for usage inside the UI
+// Params: the image URI as a string
+// Returns: a fyne.Resource representing the Image
+func GetImageResource(image_uri string) fyne.Resource {
 	r, err := fyne.LoadResourceFromURLString(image_uri) // load image
 	if err != nil {                                     // if image can not be loaded attempt to load a placeholder
 		r, err = fyne.LoadResourceFromURLString(PLACEHOLDER_IMAGE)
-		if err != nil { // if placeholder cant be loaded either, end the execution with error
-			//panic(err)
+		if err != nil { // if placeholder cant be loaded either, end the execution with error TODO: Download placeholder image, have it inside the program, do not retrieve it from online to secure failure case
+			panic(err)
 		}
 		return r
 	}
 	return r
 }
 
+// Creates a valid buildId needed for EDHRec queries
+// Param: None
+// Return: A valid buildId as a String
+func GetBuildId() string {
+	response, err := http.Get(EDHREC_BASE_URL)
+	if err != nil {
+		return "7-TtnLfoAX_AgebfCokAf"
+	} else {
+		body, err := io.ReadAll(response.Body) // read the data to a byte array
+		defer response.Body.Close()            // close the Reader once this function returns
+		if err != nil {                        // if we cant read the response, return the error
+			return "7-TtnLfoAX_AgebfCokAf"
+		} else { // if we read the response successfully
+			script_block_regex := regexp.MustCompile("<script id=\"__NEXT_DATA__\" type=\"application/json\">(.*)</script>")
+			match := script_block_regex.Find(body)
+			props_data := string(match)
+			id := gjson.Get(props_data, "buildId").String()
+			fmt.Println("ID EQUALS: " + id)
+			return id
+		}
+	}
+}
+
+// The main function that is excecuted
+// Params: None
+// Returns: Nothing
 func main() {
 	// init clipboard access
 	err := clipboard.Init()
@@ -154,7 +199,7 @@ func main() {
 	image_uri, deck_list := GetCommanderImageAndDecklist(checkGroup.Selected) // get any first commander (nothing selected)
 	// load deck into clipboard
 	clipboard.Write(clipboard.FmtText, []byte(deck_list))
-	resource := getImageResource(image_uri)
+	resource := GetImageResource(image_uri)
 	img := canvas.NewImageFromResource(resource)
 	img.FillMode = canvas.ImageFillOriginal
 
@@ -162,7 +207,7 @@ func main() {
 	new_commander := widget.NewButton("New Commander", func() {
 		image_uri, deck_list := GetCommanderImageAndDecklist(checkGroup.Selected)
 		clipboard.Write(clipboard.FmtText, []byte(deck_list))
-		resource := getImageResource(image_uri)
+		resource := GetImageResource(image_uri)
 		img.Resource = resource
 		img.Refresh()
 	})
